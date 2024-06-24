94b083f1ce5189489b5f3a543321d765cbf280ec
João Vitor Delaia <joaovdelaia@gmail.com>
2024-06-24 12:12:41 -0300

    Add project files.

---
+++ Entities/Rango.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entities;
+
+public class Rango
+{
+    [Key]/*chave primaria da tabela*/
+    public int Id { get; set; }
+
+    [Required] /*não null*/
+    [MaxLength(200)]
+    public required string Nome { get; set; }
+    public ICollection<Ingrediente> Ingredientes { get; set; } = [];
+
+    [SetsRequiredMembers]
+    public Rango(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+
+    public Rango()
+    {
+            
+    }
+
+
+}
+
